ca274fceacf5011880ca9352daffe505a958a95d
Zongren <z_zou@g.harvard.edu>
2018-11-03 12:50:17 -0400

    adding basic functions in AutoDiff.py

--- AutoDiff_CKMZ/AutoDiff_CKMZ/modules/AutoDiff.py
+++ AutoDiff_CKMZ/AutoDiff_CKMZ/modules/AutoDiff.py
@@ -1,4 +1,6 @@
-class AutoDiff:
+import numpy as np
+
+lass AutoDiff():
 	def __init__(self, x, dx = 1):
 		self.x = x
 		self.dx = dx
@@ -33,6 +35,30 @@ class AutoDiff:
 		else:
 			return AutoDiff(other)/self
 	
+	def __neg__(self):
+        return AutoDiff(-self.x, -self.dx)
+	
 	def __repr__(self):
 		print('x = {}, dx = {}'.format(self.x, self.dx))
+	
+	# basic functions
+	def sin(self, other):
+		# sine function
+		return AutoDiff(np.sin(other.x), np.cos(other.x) * other.dx)
+	
+	def cos(self, other):
+		# cosine function
+		return AutoDiff(np.cos(other.x), -np.sin(other.x) * other.dx)
+
+	def tan(self, other):
+		# tangent function
+		return AutoDiff(np.tan(other.x), other.dx / np.cos(other.x) ** 2)
+
+	def pow(self, x, other):
+		# power function:
+		if x <= 0:
+			raise Exception('Error: non-positive value for logarithm')
+		return AutoDiff(x ** other.x, x ** other.x * other.dx * np.log(x))
+
+
 		
