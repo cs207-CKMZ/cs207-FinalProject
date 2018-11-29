ca6a4094fd11af7065154aa7b33cbd96e14c6976
Malia Wenny <maliawenny@Malia.local>
2018-11-29 12:41:08 -0500

    updated test suite to include tests of individual functions (basically repeating what is seen in the doctests already

--- AutoDiff_CKMZ/AutoDiff_CKMZ/modules/tests/test_fwd.py
+++ AutoDiff_CKMZ/AutoDiff_CKMZ/modules/tests/test_fwd.py
@@ -8,6 +8,87 @@ x2 = AD.AutoDiff(3.0)
 
 tol = 1e-8
 
+def test_add():
+    f = x1 + 3
+    assert f.x == 5
+    assert f.dx == 1
+
+def test_radd():
+    f = 1 + x1
+    assert f.x == 3
+    assert f.dx == 1
+
+def test_sub():
+    f = x1 - 3
+    assert f.x == -1
+    assert f.dx == 1
+
+def test_rsub():
+    f = 3 - x1
+    assert f.x == 1
+    assert f.dx == -1
+
+def test_mul():
+    f = x1*x2
+    assert f.x == 6
+    assert f.dx == 5
+
+def test_rmul():
+    f = 3*x1
+    assert f.x == 6
+    assert f.dx == 3
+
+def test_div():
+    f = x1/x2
+    assert f.x == 2/3
+    assert f.dx == (1/3) - 2/9
+
+def test_rdiv():
+    f = 4/x1
+    assert f.x == 2
+    assert f.dx == -1
+
+def test_neg():
+    f = -x1
+    assert f.x == -2
+    assert f.dx == -1
+
+def test_pow():
+    f = x1**3
+    assert f.x == 8
+    assert f.dx == 12
+
+def test_rpow():
+    f = 3**x1
+    assert f.x == 9
+    assert tol > abs (f.dx - 9.887510598)
+
+def test_exp():
+    f = AD.exp(x1)
+    assert f.x == np.exp(2)
+    assert f.dx == np.exp(2)
+
+def test_log():
+    f = AD.log(x1)
+    assert f.x == np.log(2)
+    assert f.dx == 1/2
+
+def test_sin():
+    f = AD.sin(x1)
+    assert f.x == np.sin(2)
+    assert f.dx == np.cos(2)
+
+def test_cos():
+    f = AD.cos(x1)
+    assert f.x == np.cos(2)
+    assert f.dx == -np.sin(2)
+
+def test_tan():
+    f = AD.tan(x1)
+    assert f.x == np.tan(2)
+    assert f.dx == 1/np.cos(2)**2
+
+
 def test_pow_add():
     f = x1**3 + x1
     assert f.x == 10.0
