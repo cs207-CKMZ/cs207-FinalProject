a1d85abaf2e40707355ba9ac77d3168ec3270e66
Malia Wenny <maliawenny@Malia.local>
2018-12-10 11:14:46 -0500

    finished test suite for AD package and uploaded package to PyPI

--- AutoDiff_CKMZ/setup.py
+++ AutoDiff_CKMZ/setup.py
@@ -13,12 +13,13 @@ setuptools.setup(
         description='Automatic Differentiation package',
         long_description=long_description,
         long_description_content_type='text/markdown',
+        url = "https://github.com/cs207-CKMZ/cs207-FinalProject",
         packages=setuptools.find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
     ],
         install_requires=requirements
 
-    )
+)
