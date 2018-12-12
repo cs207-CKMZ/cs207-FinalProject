7074f6ecc6a196f7f81bcce6160f00a553e4b696
ZongrenZou <31862613+ZongrenZou@users.noreply.github.com>
2018-12-11 19:50:13 -0500

    update animation.py and rollingball.py

--- USim/USim/modules/animation.py
+++ USim/USim/modules/animation.py
@@ -1,44 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import modules.rollingball as RB
+import rollingball as RB
+import AutoDiff_CKMZ.modules.AutoDiff as AD
 from time import time
-from AutoDiff_CKMZ.modules.AutoDiff import *
 
-function_list = [lambda x: AutoDiff.sin(x), 
-                 lambda x: AutoDiff.exp(x), 
-                 lambda x: x * AutoDiff.sin(x), 
-                 lambda x: -AutoDiff.log(x), 
-                 lambda x: AutoDiff.sinh(x), 
-                 lambda x: AutoDiff.exp(AutoDiff.cos(x)),
+function_list = [lambda x: AD.sin(x), 
+                 lambda x: AD.exp(x), 
+                 lambda x: x * AD.sin(x), 
+                 lambda x: - AD.log(x), 
+                 lambda x: AD.sinh(x), 
+                 lambda x: AD.exp(AD.cos(x)),
                  lambda x: 2 ** x, 
-                 lambda x: 1 / AutoDiff.tan(x), 
-                 lambda x: AutoDiff.sin(x) / x, 
+                 lambda x: 1 / AD.tan(x), 
+                 lambda x: AD.sin(x) / x, 
                  lambda x: 1 / x,
                  lambda x: x ** 4]
 
-derivative_list = [lambda x: cos(x), 
-                 lambda x: exp(x), 
-                 lambda x: sin(x) + x * cos(x), 
-                 lambda x: -1 / x, 
-                 lambda x: cosh(x), 
-                 lambda x: - exp(cos(x)) * sin(x),
-                 lambda x: 2 ** x * log(2), 
-                 lambda x: - 1 / sin(x) ** 2, 
-                 lambda x: (x * cos(x) - sin(x)) / x ** 2, 
-                 lambda x: - 1 / x ** 2,
-                 lambda x: 4 * x ** 3]
+
+derivative_list = [lambda x: np.cos(x), 
+                   lambda x: np.exp(x), 
+                   lambda x: np.sin(x) + x * np.cos(x), 
+                   lambda x: -1 / x, 
+                   lambda x: np.cosh(x), 
+                   lambda x: - np.exp(np.cos(x)) * np.sin(x),
+                   lambda x: 2 ** x * np.log(2), 
+                   lambda x: - 1 / np.sin(x) ** 2, 
+                   lambda x: (x * np.cos(x) - np.sin(x)) / x ** 2, 
+                   lambda x: - 1 / x ** 2,
+                   lambda x: 4 * x ** 3]
 
 class function():
-    def __init__(self, function_list=None):
+    def __init__(self, function_list=None, AD=False):
         self.function_list = function_list
         self.num = len(function_list)
+        self.AD = AD
     
     def __getitem__(self, key):
         try: 
             return self.function_list[key]
         except IndexError:
-            raise UserWarning('Error, please pick one of the given functions.')
+            raise UserError('function index out of range')
 
 function_set = function(function_list)
 derivative_set = function(derivative_list)
@@ -48,6 +50,7 @@ class Animation():
                  init_status=(0, 0), 
                  x_range=(0, 4), 
                  option=1, 
+                 friction=0,
                  dt=0.0001):
         self.curve = function_set[function_index]
         self.gradient = None
@@ -57,6 +60,7 @@ class Animation():
         self.y0 = self.curve(self.x0)
         self.xmin, self.xmax = x_range
         self.option = option
+        self.friction = friction
         self.rollingball = None
         self.dt = dt
         
@@ -67,19 +71,25 @@ class Animation():
         self.rollingball = RB.rollingball(curve=self.curve, 
                                           gradient=self.gradient, 
                                           init_status=(self.x0, self.y0, self.v0), 
-                                          option=self.option)
+                                          option=self.option,
+                                          friction=self.friction)
         # for animation setting
         self.fig = plt.figure()
         self.x_range = np.linspace(self.xmin, self.xmax, 1000)
         self.y_range = self.curve(self.x_range)
-        self.ax = self.fig.add_subplot(111, autoscale_on=True,xlim=(self.xmin, self.xmax), ylim=(min(self.y_range), max(self.y_range)))
+        self.ymin = min(self.y_range)
+        self.ymax = max(self.y_range)
+        self.ax = self.fig.add_subplot(111, 
+                                  autoscale_on=True,
+                                  xlim=(self.xmin, self.xmax), 
+                                  ylim=(self.ymin, self.ymax))
         self.ax.grid()
         self.lines = []
         self.lines.append(self.ax.plot([], [], lw=0.5)[0])
         self.lines.append(self.ax.plot([], [], 'o-', lw=2)[0])
         self.time_text = self.ax.text(0.02, 0.95, '', transform=self.ax.transAxes)
         self.velocity_text = self.ax.text(0.02, 0.90, '', transform=self.ax.transAxes)
-        self.instruction_text = self.ax.text(0.02, 0.85, '', transform=self.ax.transAxes)
+        self.instruction_text = self.ax.text(0.02, 0.80, '', transform=self.ax.transAxes)
     
     def animation_init(self):
         # initialize animation
@@ -103,6 +113,12 @@ class Animation():
         while count < frames_rate and not self.pause:
             self.rollingball.step(self.dt)
             count += 1
+        # check if ball is still in range
+        x, y = self.rollingball.position()
+        if x < self.xmin or x > self.xmax or y < self.ymin or y > self.ymax:
+            self.instruction_text.set_text('Out of range! Animation terminates automatically.')
+            plt.pause(3)
+            plt.close()
         self.lines[1].set_data(*self.rollingball.position())
 
         self.time_text.set_text('time = %.1f' % self.rollingball.time_elapsed)
@@ -112,10 +128,7 @@ class Animation():
         return self.lines, self.time_text, self.velocity_text, self.instruction_text
     
     def run_animation(self):
-        t0 = time()
-        self.animate(0)
-        t1 = time()
-        interval = 1000 * self.dt - (t1 - t0)
+        interval = 1000 * self.dt
         
         self.fig.canvas.mpl_connect('button_press_event', self.onClick)
         self.fig.canvas.mpl_connect('key_press_event', self.onKey)
--- USim/USim/modules/rollingball.py
+++ USim/USim/modules/rollingball.py
@@ -1,22 +1,22 @@
-from AutoDiff_CKMZ.modules.AutoDiff import *
+import AutoDiff_CKMZ.modules.AutoDiff as AD
 
 class rollingball:
-    def __init__(self, init_status=(1, 1, 0), G=9.8, curve=0, gradient=0, option=0):
+    
+    def __init__(self, init_status=(1, 1, 0), G=9.8, curve=0, gradient=None, option=0, friction=0, tol=1e-1):
         self.x, self.y, self.v = init_status # intial conditions: initial position, initial velocity
         self.curve = curve # curve function
         self.time_elapsed = 0
         self.G = G # g
         if gradient == None and option == 0:
-            raise UserWarning('Error: No analytic derivative function provided!')
+            raise Exception('Error: No analytic derivative function provided!')
         self.gradient = gradient # analytic derivative function
         self.option = option # 0 for analytic derivative, 1 for AD, 2 for numerical method, for derivative
-        self.ontrack = 1
-        df_x = self.slope(self.x)
-        sin_theta = df_x / (1 + df_x ** 2) ** 0.5
-        cos_theta = 1 / (1 + df_x ** 2) ** 0.5
-        self.vx = self.v * cos_theta
-        self.vy = self.v * sin_theta
-
+        if friction < 1 and friction >= 0:
+            self.friction = friction
+        else:
+            raise UserError('Wrong friction coefficient: should be from 0 to 1')
+        self.tol = tol
+           
     def position(self):
         return (self.x, self.y)
     
@@ -28,65 +28,47 @@ class rollingball:
             return self.gradient(x)
         if self.option == 1:
             # implement AD here
-            obj = AutoDiff(x)
+            obj = AD.AutoDiff(x)
             return self.curve(obj).dx
         if self.option == 2:
-            h = 1e-2  # 'dx' or step size for numerical approximation
+            # implement numerical method here
+            h = 1e-2
             return (self.curve(x + h) - self.curve(x)) / h
     
     def acceleration(self, x):
         df_x = self.slope(x)
-        a = - self.G * df_x / (1 + df_x ** 2) ** 0.5
+        a = - self.G * df_x / (1 + df_x ** 2) ** 0.5 - self.sign() * self.friction * self.G / (1 + df_x ** 2) ** 0.5
         
         return a
-
-    def ont(self, dt):
-        a = self.acceleration(self.x)
-        df_x = self.slope(self.x)
+    
+    def sign(self):
+        if self.v > 0:
+            return 1
+        elif self.v < 0:
+            return -1
+        else:
+            return 0
+    
+    def step(self, dt):
+        self.time_elapsed += dt
+        x = self.x
+        y = self.y
+        a = self.acceleration(x)
+        df_x = self.slope(x)
         sin_theta = df_x / (1 + df_x ** 2) ** 0.5
         cos_theta = 1 / (1 + df_x ** 2) ** 0.5
-
+        
+        
         # update s and velocity
-        tmp_v = self.v + a * dt
+        self.v += a * dt
         delta_s = self.v * dt
         delta_x = delta_s * cos_theta
         delta_y = delta_s * sin_theta
-        if (self.y + delta_y <= self.curve(self.x+delta_x)):
-            self.v = tmp_v
-            self.x += delta_x
-            self.y += delta_y
-            self.vx = self.v * cos_theta
-            self.vy = self.v * sin_theta
-        else:
-            self.offt(dt)
-            self.ontrack = 0
-
-    def offt(self, dt):
-        a = -self.G
-        delta_x = self.vx * dt
-        delta_y = self.vy * dt + 0.5 * a * dt ** 2
-        if (self.y + delta_y > self.curve(self.x + delta_x)):
-            self.vy += a * dt
-            self.v = (self.vx ** 2 + self.vy ** 2) ** 0.5
-            self.x += delta_x
-            self.y += delta_y
-        else:
-            self.x += delta_x
-            self.y = self.curve(self.x)
-            df_x = self.slope(self.x)
-            sin_theta = df_x / (1 + df_x ** 2) ** 0.5
-            cos_theta = 1 / (1 + df_x ** 2) ** 0.5
-            tmp_vx = self.vx
-            tmp_vy = self.vy + a * dt
-            self.v = tmp_vx * cos_theta + tmp_vy * sin_theta
-            self.vx = self.v * cos_theta
-            self.vy = self.v * sin_theta
-            self.ontrack = 1
-    
-    def step(self, dt):
-        if (self.ontrack == 0):
-            self.offt(dt)
-        else:
-            self.ont(dt)
-        self.time_elapsed += dt
-
+        # update x and y
+        self.x += delta_x
+        self.y += delta_y
+        
+        # check if the ball is still on the curve
+        y_curve = self.curve(self.x)
+        if abs(y_curve - self.y) > self.tol:
+            raise UserError('Ball flies off. Animation terminates.')
